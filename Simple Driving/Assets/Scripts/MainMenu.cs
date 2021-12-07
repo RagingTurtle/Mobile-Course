1d0b853c4fb241d9f7714a6447478f7dfe9a59a1
RagingTurtle <tinybit210r@gmail.com>
2021-12-07 10:43:42 -0600

    Added energy system

--- Simple Driving/Assets/Scripts/MainMenu.cs
+++ Simple Driving/Assets/Scripts/MainMenu.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text enegryText;
+    [SerializeField] int maxEnergy;
+    [SerializeField] int energyRecharge;
+    int energy;
+    const string energyKey = "Energy";
+    const string energyReadyKey = "EnergyReady";
     private void Start()
     {
         scoreText.text = "High Score:\n" + PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0).ToString();
+
+        energy = PlayerPrefs.GetInt(energyKey, maxEnergy);
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(energyReadyKey, string.Empty);
+            if (energyReadyString == string.Empty) { return; }
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(energyKey, energy);
+            }
+        }
+        enegryText.text = "Play (" + energy + ")";
     }
     public void Play()
     {
+        if (energy <= 0) { return; };
+        energy--;
+        PlayerPrefs.SetInt(energyKey, energy);
+        if (energy <= 0)
+        {
+            PlayerPrefs.SetString(energyReadyKey, DateTime.Now.AddMinutes(energyRecharge).ToString());
+        }
         SceneManager.LoadScene("Game");
     }
 }
