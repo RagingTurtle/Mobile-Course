cf4c25da16d136d8037ad245695d73c53275790e
RagingTurtle <tinybit210r@gmail.com>
2021-12-07 14:50:14 -0600

    Ship wraps around screen

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,37 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPostion = transform.position;
+        Vector3 viewPortPosition = Camera.main.WorldToViewportPoint(transform.position);
+        if (viewPortPosition.x > 1)
+        {
+            newPostion.x = -newPostion.x + .1f;
+        }
+        else if (viewPortPosition.x < 0)
+        {
+            newPostion.x = -newPostion.x - .1f;
+        }
+
+        if (viewPortPosition.y > 1)
+        {
+            newPostion.y = -newPostion.y + .1f;
+        }
+        else if (viewPortPosition.y < 0)
+        {
+            newPostion.y = -newPostion.y - .1f;
+        }
+        newPostion.z = 0f;
+        transform.position = newPostion;
+    }
+
+    private void ProcessInput()
+    {
         if (!Touchscreen.current.press.isPressed)
         {
             movementDirection = Vector3.zero;
@@ -30,6 +62,7 @@ public class PlayerMovement : MonoBehaviour
             movementDirection.Normalize();
         }
     }
+
     private void FixedUpdate()
     {
         if (movementDirection == Vector3.zero) { return; }
